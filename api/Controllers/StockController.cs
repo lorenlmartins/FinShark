5d0e40f8e23724e22d4b6336af670bb7b9188dd3
Loren Lima Martins <loren.lima@take.net>
2024-06-29 15:42:31 -0300

    feat: add put

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -6,7 +6,6 @@ using api.Data;
 using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
-
 namespace api.Controllers
 {
     [Route("api/stock")]
@@ -35,7 +34,6 @@ namespace api.Controllers
             }
             return Ok(stock.ToStockDto());
         }
-
         [HttpPost]
         public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
         {
@@ -43,6 +41,29 @@ namespace api.Controllers
             _context.Stocks.Add(stockModel);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
         }
 
     }
