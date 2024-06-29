e67ae845433d662cae6d67a2056680a88f3bd575
Loren Lima Martins <loren.lima@take.net>
2024-06-29 09:40:41 -0300

    feat: useEffect

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 export interface SearchResponse {
   data: CompanySearch[];
 }
-
 export const searchCompanies = async (query: string) => {
   try {
     const data = await axios.get<SearchResponse>(
@@ -19,5 +18,16 @@ export const searchCompanies = async (query: string) => {
       console.log("unexpected error: ", error);
       return "An expected error has occured.";
     }
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
   }
 };
