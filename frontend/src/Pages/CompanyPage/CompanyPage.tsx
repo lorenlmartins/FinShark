68eb78301527a519339ac1bf05a45bac8493da5e
Loren Lima Martins <loren.lima@take.net>
2024-06-29 12:50:11 -0300

    feat: dashboard

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -2,12 +2,17 @@ import React, { useEffect, useState } from "react";
 import { CompanyProfile } from "../../company";
 import { useParams } from "react-router-dom";
 import { getCompanyProfile } from "../../api";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../Components/Tile/Tile";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
   let { ticker } = useParams();
+
   const [company, setCompany] = useState<CompanyProfile>();
+  const [activeSidebarItem, setActiveSideBarItem] = useState<number>(1);
 
   useEffect(() => {
     const getProfileInit = async () => {
@@ -16,16 +21,19 @@ const CompanyPage = (props: Props) => {
     };
     getProfileInit();
   }, []);
-
   return (
     <>
       {company ? (
-        <div className="company-profile-container">{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard>
+            <Tile title="Company Name" subTitle={company.companyName} />
+          </CompanyDashboard>
+        </div>
       ) : (
         <div>Company Not Found!</div>
       )}
     </>
   );
 };
-
 export default CompanyPage;
