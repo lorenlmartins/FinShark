bc2512e2e067b39d86641a61dcfd54ec2f5d7f09
Loren Lima Martins <loren.lima@take.net>
2024-06-29 07:20:57 -0300

    feat: forms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,18 @@ function App() {
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
-  const onClick = async (e: SyntheticEvent) => {
+
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    console.log(e);
+    //Arrays are tricky to update. Please wait till next video.
+  };
+
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
     const result = await searchCompanies(search);
     //setServerError(result.data);
     if (typeof result === "string") {
@@ -22,12 +30,19 @@ function App() {
       setSearchResult(result.data);
     }
   };
+
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange} />
-      <CardList searchResults={searchResult} />
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        handleSearchChange={handleSearchChange}
+      />
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
       {serverError && <div>Unable to connect to API</div>}
-      {/* {serverError ? <div>Connected</div> : <div>Unable to connect to api</div>} */}
     </div>
   );
 }
