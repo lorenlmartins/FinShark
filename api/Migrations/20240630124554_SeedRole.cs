c7ebd6f9ad8bcc3289a79674f7e8504dc85af7c2
Loren Lima Martins <loren.lima@take.net>
2024-06-30 09:48:36 -0300

    feat: add registration

---
+++ api/Migrations/20240630124554_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "5245f6cd-9798-49c3-aaf0-45b90ef25308", null, "User", "USER" },
+                    { "bcd2d111-7db3-481a-bcf6-eb4beb9fb45b", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5245f6cd-9798-49c3-aaf0-45b90ef25308");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bcd2d111-7db3-481a-bcf6-eb4beb9fb45b");
+        }
+    }
+}
