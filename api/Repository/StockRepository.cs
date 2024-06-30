193fe17cdc9fc67ab46162a4cac94d4e26f89049
Loren Lima Martins <loren.lima@take.net>
2024-06-30 09:28:40 -0300

    feat: sorting

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -34,19 +34,24 @@ namespace api.Repository
             await _context.SaveChangesAsync();
             return stockModel;
         }
-
         public async Task<List<Stock>> GetAllAsync(QueryObject query)
         {
             var stocks = _context.Stocks.Include(c => c.Comments).AsQueryable();
-
             if (!string.IsNullOrWhiteSpace(query.CompanyName))
             {
                 stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
-
             if (!string.IsNullOrWhiteSpace(query.Symbol))
             {
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDecsending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
             }
 
             return await stocks.ToListAsync();
