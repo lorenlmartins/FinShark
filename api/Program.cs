7b301ec09ef02c25b26da58c13a8214df4fb8f53
Loren Lima Martins <loren.lima@take.net>
2024-06-30 08:35:57 -0300

    feat: comment system

--- api/Program.cs
+++ api/Program.cs
@@ -2,7 +2,6 @@ using api.Data;
 using api.Interfaces;
 using api.Repository;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +14,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 });
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
